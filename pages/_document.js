26133ec80d37aea5162def26a06d20567edb12f7
beckyhu163-cmyk <beckyhu163@gmail.com>
2026-05-28 15:16:15 +0800

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="zh">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="然er和风 - 然er的专属空间"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
